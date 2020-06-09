e3b5433c4aec6c198e3f51485ea303653897af0c
Joseph Cottin <joseph.cottin@kedgebs.com>
2020-06-09 11:48:05 +0200

    creation of the views for seasons, matches, forecasts

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,6 +1,4 @@
 class MatchesController < ApplicationController
-
-def index
-end
-
+  def index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
     resources :forecasts, only: [:new, :create, :show, :confirm_pending]
   end
   resources :forecasts, only: [:destroy]
+  resources :seasons, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
