d2f307c6f65213f3bedab00b2922df9458d552e1
Joseph Cottin <joseph.cottin@kedgebs.com>
2020-06-09 11:19:40 +0200

    creation of match and forecast controller and routes

---
+++ app/controllers/forecasts_controller.rb
@@ -0,0 +1,16 @@
+class ForecastsController < ApplicationController
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def confirm_pending
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
+  resources :matches, only: [:index] do
+    resources :forecasts, only: [:new, :create, :show, :confirm_pending]
+  end
+  resources :forecasts, only: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
