c3e1e7659eaa3a3007cf7beee610b189995d9c39
Clement Guibaud <clement.guibaud@kedgebs.com>
2020-06-13 16:18:23 +0200

    fix bug

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,8 +1,8 @@
 require_relative '../models/sport_odd'
 class MatchesController < ApplicationController
   def index
-    SportOdd.all_matches_for_week
-    SportOdd.sort
+    # SportOdd.all_matches_for_week
+    # SportOdd.sort
     @matches = Match.all
   end
 end
