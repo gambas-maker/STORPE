b49588d466dd35006f6f7398627440f953ed7160
Clement Guibaud <clement.guibaud@kedgebs.com>
2020-06-15 09:31:16 +0200

    fix bug, production sidekiq

--- app/jobs/fetchapi_job.rb
+++ app/jobs/fetchapi_job.rb
@@ -2,8 +2,8 @@ class FetchapiJob < ApplicationJob
   queue_as :default
 
   def perform
-    puts "I'm gonna work"
-    sleep 10
+    puts "I'm going to create the weekly games"
+    SportOdd.all_matches_for_week
     puts "I'm done"
   end
 end
