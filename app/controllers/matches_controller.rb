93125f21fc48d53167db8944603395aee9838507
Joseph Cottin <joseph.cottin@kedgebs.com>
2020-06-17 16:52:00 +0200

    droplist fonctionnel mais design de mort

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -4,5 +4,14 @@ class MatchesController < ApplicationController
     @forecasts = Forecast.where(confirmed: false)
     @matches = Match.all
     @player = 1
+    league_filters
+  end
+
+  def league_filters
+    if params[:query].present?
+      @matches = Match.where(country: params[:query])
+    else
+      @matches = Match.all
+    end
   end
 end
