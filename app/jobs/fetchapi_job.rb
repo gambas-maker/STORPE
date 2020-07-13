3e1ea55de53ccd9925f21d75568c00ebbc078512
Clement Guibaud <clement.guibaud@kedgebs.com>
2020-07-13 17:58:59 +0200

    done with heroku scheduler

--- app/jobs/fetchapi_job.rb
+++ app/jobs/fetchapi_job.rb
@@ -1,8 +1,10 @@
 require 'sidekiq-scheduler'
 
 class FetchapiJob < ApplicationJob
+  def perform(scheduled_time)
+    day_of_the_week = Time.at(scheduled_time).wday
+    return unless (1 || 3).include?(day_of_the_week)
 
-  def perform
     puts "I'm going to create games for the next two days"
     SportOdd.matches_for_four_days
     puts "I'm done with matches from Mon/Wed to Tue/Thu"
