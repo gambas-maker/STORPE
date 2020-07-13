a063b208ec998b9df0763ebb8f115362455f7c94
Clement Guibaud <clement.guibaud@kedgebs.com>
2020-07-13 23:14:27 +0200

    changement de stratégie, utilisation sidekiq.yml car bonne manière de l'écrire

--- app/jobs/fetchapi_job.rb
+++ app/jobs/fetchapi_job.rb
@@ -1,9 +1,7 @@
 require 'sidekiq-scheduler'
 
 class FetchapiJob < ApplicationJob
-  def perform(scheduled_time)
-    day_of_the_week = Time.at(scheduled_time).wday
-    return unless (1 || 3).include?(day_of_the_week)
+  def perform
 
     puts "I'm going to create games for the next two days"
     SportOdd.matches_for_four_days
