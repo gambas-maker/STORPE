1a48bb3d7e20b8667ec69942252e71f8678e300b
Clement Guibaud <clement.guibaud@kedgebs.com>
2020-07-14 22:02:38 +0200

    ajouter dans les modèles has many et belongs to

--- app/models/championship.rb
+++ app/models/championship.rb
@@ -1,3 +1,4 @@
 class Championship < ApplicationRecord
   belongs_to :season
+  has_many :player_seasons
 end
--- app/models/player_season.rb
+++ app/models/player_season.rb
@@ -1,6 +1,7 @@
 class PlayerSeason < ApplicationRecord
   belongs_to :user
   belongs_to :season
+  belongs_to :championship
   has_many :forecasts
   has_many :matches, through: :forecasts
 end
