c0e15781682d79e280215a1e9c2e9a07bfc42a28
Clement Guibaud <clement.guibaud@kedgebs.com>
2020-07-18 09:20:19 +0200

    changement dans le schema de db result = integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_16_143041) do
+ActiveRecord::Schema.define(version: 2020_07_17_211038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2020_07_16_143041) do
   end
 
   create_table "matches", force: :cascade do |t|
-    t.string "result"
+    t.integer "result"
     t.string "team_home"
     t.string "team_away"
     t.string "sport"
