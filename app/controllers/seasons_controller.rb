92511e6dee9f5210baa7dfd0b6eb57ff8a098ef8
Clement Guibaud <clement.guibaud@kedgebs.com>
2020-07-23 23:28:17 +0200

    classement optimizé et présentable

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -2,7 +2,8 @@ class SeasonsController < ApplicationController
   def show
     @championships = Championship.where(season_id: Season.last.id)
     @playerseason = PlayerSeason.where(user_id: current_user.id)
-    @playerseasons = PlayerSeason.where(season_id: Season.last.id)
+    @championship = Championship.where(id: current_user.player_seasons[0].championship.id)
+    @playerseasons = PlayerSeason.where(championship_id: current_user.player_seasons[0].championship.id)
     @season = Season.last
     @forecasts = Forecast.where(confirmed: true)
   end
