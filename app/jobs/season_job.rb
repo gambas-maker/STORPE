92d63a98f98cb7188eefda2592dd8a381e290143
Clement Guibaud <clement.guibaud@kedgebs.com>
2020-07-24 16:38:00 +0200

    classement bon même si pas forecast

--- app/jobs/season_job.rb
+++ app/jobs/season_job.rb
@@ -4,13 +4,14 @@ class SeasonJob < ApplicationJob
   def perform
     puts "New season is coming!"
     @season = Season.create!
-    puts "New is season is ready"
+    puts "New season is ready"
     PlayerSeason.update_all(season_id: @season.id)
     Championship.update_all(season_id: @season.id)
     @playerseasons = PlayerSeason.where(season_id: Season.last.id)
     @playerseasons.each do |playerseason|
       playerseason.update(number_of_points: 0)
     end
-    puts "All player seasons are updated"
+    puts "All player seasons are updated same for Championship"
+
   end
 end
