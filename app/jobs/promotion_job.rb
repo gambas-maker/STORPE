70267276dde0762005d05757c5827c4aa4f72f0c
Clement Guibaud <clement.guibaud@kedgebs.com>
2020-07-26 16:28:50 +0200

    début solution pour répondre à la question

--- app/jobs/promotion_job.rb
+++ app/jobs/promotion_job.rb
@@ -6,13 +6,17 @@ class PromotionJob < ApplicationJob
     @playerseasons = PlayerSeason.all
     @forecasts = Forecast.where(season_id: Season.last.id)
     @championships.each do |championship|
-      championship.player_seasons.each do |playerseason|
-        if playerseason.forecasts.exists?
-          puts playerseason.championship
-        else
-          # puts "Dans les GLORRYYYY HOOOOOOOLE"
+      puts championship.player_seasons.sort
+      # championship.player_seasons.each do |playerseason|
+      #   if playerseason.forecasts.exists?
+      #     championship.player_seasons.sort_by { |number| number }
+      # puts championship.player_seasons
+        # playerseason.sort_by
+        # if playerseason.forecasts.exists?
+        # else
+        # puts "Dans les GLORRYYYY HOOOOOOOLE"
         end
       end
     end
-  end
-end
+  # end
+# end
