1a5ef6e9961a8542c19ebb379e19bd12f5698a5c
Clement Guibaud <clement.guibaud@kedgebs.com>
2020-07-27 16:16:28 +0200

    par championnat les joueurs sont triés par performance, il manque uniquement à savoir si ils ont pronostiqué cette semaine pour récupérer les 4 premiers et derniers

--- app/jobs/promotion_job.rb
+++ app/jobs/promotion_job.rb
@@ -6,17 +6,26 @@ class PromotionJob < ApplicationJob
     @playerseasons = PlayerSeason.all
     @forecasts = Forecast.where(season_id: Season.last.id)
     @championships.each do |championship|
-      puts championship.player_seasons.sort
+      ranking = {}
+      championship.player_seasons.forecasts.where(season_id: Season.last.id).each do |playerseason|
+      if playerseason.forecasts.exists?
+        puts "hello"
+        championship.player_seasons.each { |hash| ranking[hash] = hash.number_of_points }
+        ranking.keys.sort_by { |key| ranking[key] }.reverse.each do |key|
+          puts ranking[key]
+        end
+      # championship.player_seasons.each do |playerseason|
+      # puts Hash
       # championship.player_seasons.each do |playerseason|
       #   if playerseason.forecasts.exists?
       #     championship.player_seasons.sort_by { |number| number }
       # puts championship.player_seasons
         # playerseason.sort_by
         # if playerseason.forecasts.exists?
-        # else
+      else
         # puts "Dans les GLORRYYYY HOOOOOOOLE"
-        end
+      end
       end
     end
-  # end
-# end
+  end
+end
