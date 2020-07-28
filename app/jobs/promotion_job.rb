96f4e3bd8274b5fb58c1782041b0200987f09db8
Clement Guibaud <clement.guibaud@kedgebs.com>
2020-07-28 19:15:49 +0200

    pb update championnat pour le joueur et création d'un championnat si le championnat n'existe pas

--- app/jobs/promotion_job.rb
+++ app/jobs/promotion_job.rb
@@ -12,14 +12,16 @@ class PromotionJob < ApplicationJob
         ranking.sort_by { |k, v| v }.reverse.first(4)
         puts ranking.sort_by { |k, v| k.forecasts.where(season_id: Season.last.id).exists? v }.reverse.last(4)
         if championship.name == "CFA"
-          if Championship.where(name: "LDC").exists? && Championship.where(name: "LDC").playerseasons.count < 20
+          puts championship
+          if championship.where(name: "LDC").exists? && championship.player_seasons.count < 20
+            puts champion.player_seasons
             ranking.each { |x| x.update(championship_id: Championship.where(name: "LDC")) }
           else
             Championship.create!(name: "LDC", season_id: Season.last.id)
             puts ranking.class
             ranking.each { |x| puts x }
           end
-        elsif championship.name == "LDC"
+        else
         end
       else
         array = []
