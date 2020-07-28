94c56b223684f712fb70b76d76cc7532061a37a2
Clement Guibaud <clement.guibaud@kedgebs.com>
2020-07-28 13:00:39 +0200

    deux pb, update la sélection et vérifier que le championnat possède assez ou trop de joueurs

--- app/jobs/promotion_job.rb
+++ app/jobs/promotion_job.rb
@@ -8,12 +8,24 @@ class PromotionJob < ApplicationJob
     @championships.each do |championship|
       ranking = {}
       championship.player_seasons.each { |hash| ranking[hash] = hash.number_of_points }
-      if championship.player_seasons.forecast.where(season_id: Season.last.id).count >= 8
-        puts ranking.sort_by { |k, v| v }.reverse.first(4)
+      if championship.player_seasons.count >= 8
+        ranking.sort_by { |k, v| v }.reverse.first(4)
         puts ranking.sort_by { |k, v| k.forecasts.where(season_id: Season.last.id).exists? v }.reverse.last(4)
+        if championship.name == "CFA"
+          if Championship.where(name: "LDC").exists? && Championship.where(name: "LDC").playerseasons.count < 20
+            ranking.each { |x| x.update(championship_id: Championship.where(name: "LDC")) }
+          else
+            Championship.create!(name: "LDC", season_id: Season.last.id)
+            puts ranking.class
+            ranking.each { |x| puts x }
+          end
+        elsif championship.name == "LDC"
+        end
       else
-        puts ranking.sort_by { |k, v| v }.reverse.first(2)
-        puts ranking.sort_by { |k, v| k.forecasts.where(season_id: Season.last.id).exists? v }.reverse.last(2)
+        array = []
+        array_1 = []
+        puts array << ranking.sort_by { |k, v| v }.reverse.first(2)
+        puts array_1 << ranking.sort_by { |k, v| k.forecasts.where(season_id: Season.last.id).exists? v }.reverse.last(2)
       end
     end
   end
