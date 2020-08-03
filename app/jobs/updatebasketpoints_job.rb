b8d697742ad98796b2861703558db2f76aa3abdb
Clement Guibaud <clement.guibaud@kedgebs.com>
2020-08-03 16:37:33 +0200

    decrease le scope d'appelle des matchs de basket

--- app/jobs/updatebasketpoints_job.rb
+++ app/jobs/updatebasketpoints_job.rb
@@ -2,7 +2,8 @@ class UpdatebasketpointsJob < ApplicationJob
   queue_as :default
 
   def perform
-    @matches = Match.where(sport: "basket", points_home: nil)
+    @matches = Match.where(sport: "basketball", points_home: nil)
+    puts @matches.count
     @matches.each do |match|
       BasketballTomorrow.get_odds_for_match(match)
     end
