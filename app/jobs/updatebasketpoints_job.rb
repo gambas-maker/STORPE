cedf356aeafc635b56f499ff9371eb16d27bbfb3
Clement Guibaud <clement.guibaud@kedgebs.com>
2020-08-03 16:12:35 +0200

    création d'un job qui viendra update les points des matchs qui sont crées mais dont les cotes sont pas encore disponibles, reste à déterminer le moment où le réaliser

--- app/jobs/updatebasketpoints_job.rb
+++ app/jobs/updatebasketpoints_job.rb
@@ -2,7 +2,7 @@ class UpdatebasketpointsJob < ApplicationJob
   queue_as :default
 
   def perform
-    @matches = Match.where(sport: "basket")
+    @matches = Match.where(sport: "basket", points_home: nil)
     @matches.each do |match|
       BasketballTomorrow.get_odds_for_match(match)
     end
