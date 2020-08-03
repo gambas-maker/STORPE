1f0589e84d6b831320f411846e5f56532256164c
Clement Guibaud <clement.guibaud@kedgebs.com>
2020-08-03 16:37:53 +0200

    remove puts matches.count

--- app/jobs/updatebasketpoints_job.rb
+++ app/jobs/updatebasketpoints_job.rb
@@ -3,7 +3,6 @@ class UpdatebasketpointsJob < ApplicationJob
 
   def perform
     @matches = Match.where(sport: "basketball", points_home: nil)
-    puts @matches.count
     @matches.each do |match|
       BasketballTomorrow.get_odds_for_match(match)
     end
