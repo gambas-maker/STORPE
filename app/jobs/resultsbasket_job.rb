8a4503bc535ed87ba5a000e4b3f596f3e40f2cf2
Clement Guibaud <clement.guibaud@kedgebs.com>
2020-08-04 10:27:40 +0200

    mise en place du job pour calculer les points NBA, récup les résulats et donner les points en questions à la bonne personne

--- app/jobs/resultsbasket_job.rb
+++ app/jobs/resultsbasket_job.rb
@@ -8,5 +8,7 @@ class ResultsbasketJob < ApplicationJob
       Basketballmatch.get_results_for_match(game)
     end
     puts "All the results are gathered"
+    CalculatepointsbasketJob.perform_now
+    RecalcpointsJob.perform_now
   end
 end
