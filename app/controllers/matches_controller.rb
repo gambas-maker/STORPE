25f3adbdcfcddef9d9e258c767b7ac8f69d095a5
Clement Guibaud <clement.guibaud@kedgebs.com>
2020-08-05 16:08:49 +0200

    filtre fonctionnel

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -7,18 +7,10 @@ class MatchesController < ApplicationController
   end
 
   def league_filters
-    if params[:sport] == "Football"
-      if params[:query].present?
-        @matches = Match.where(country: params[:query])
-      else
-        @matches = Match.where(sport: "football")
-      end
-    else params[:sport] == "Basketball"
-      if params[:query].present?
-        @matches = Match.where(league: params[:query])
-      else
-        @matches = Match.where(sport: "basketball")
-      end
+    if params[:query].present?
+      @matches = Match.where(league: params[:query])
+    else
+      @matches
     end
   end
 end
