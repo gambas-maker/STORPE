c3eb54399b71b5b4a9a14b74f223f8ed3e1e6bda
Clement Guibaud <clement.guibaud@kedgebs.com>
2020-08-06 10:57:54 +0200

    changement appelle résultats NBA

--- app/jobs/resultsbaskettoday_job.rb
+++ app/jobs/resultsbaskettoday_job.rb
@@ -5,7 +5,7 @@ class ResultsbaskettodayJob < ApplicationJob
     puts "Let's get the basketball results"
     @matches = Match.where(event_stamp: Date.today.to_s, sport: "basketball")
     @matches.each do |game|
-      Basketballmatch.get_results_for_match(game)
+      BasketballTomorrow.get_results_for_match(game)
     end
     puts "All the results are gathered"
     CalculatepointsbasketJob.perform_now
