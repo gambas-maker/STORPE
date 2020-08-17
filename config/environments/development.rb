ee8a3d9b74bfbe14977af64218b14662e2294f52
Clement Guibaud <clement.guibaud@kedgebs.com>
2020-08-17 13:57:30 +0200

    ok move forwward

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_options = {host: 'contactstorpe@gmail.com'}
+  config.action_mailer.default_options = {from: 'contactstorpe@gmail.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
