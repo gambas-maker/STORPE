f72e5b7939679ba29dc2e6f9600b0e4b1423086e
Clement Guibaud <clement.guibaud@kedgebs.com>
2020-08-17 10:44:30 +0200

    mail marketing après la connection

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,7 @@
 class UserMailer < ApplicationMailer
-  default from: 'clement.guibaud@storpe.com'
-
-  def welcome_email(user_id)
-    @user = User.find(user_id)
-    email_with_name = %("#{@user.pseudo}" <#{@user.email}>)
-    mail(to: email_with_name, subject: 'Bienvenue chez STORPE ⚽ 🏀!')
+  def welcome_email(user)
+    @user = user
+    # email_with_name = %("#{@user.pseudo}" <#{@user.email}>)
+    mail(to: @user, subject: 'Bienvenue chez STORPE ⚽ 🏀!')
   end
 end
