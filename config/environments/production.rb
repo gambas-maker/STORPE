463627d9ea4bb8c5b3eaf400d637c48327591b9a
Clement Guibaud <clement.guibaud@kedgebs.com>
2020-08-17 17:33:12 +0200

    changement de :smtp à :sendmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: "www.storpe.club" }
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
