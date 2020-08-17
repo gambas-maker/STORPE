744e87402d6742e386892c01fcc9b068077989c7
Clement Guibaud <clement.guibaud@kedgebs.com>
2020-08-17 19:11:54 +0200

    changement car erreur de syntaxe

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,8 +70,7 @@ Rails.application.configure do
 #   config.action_mailer.raise_delivery_errors = false
 #   config.action_mailer.default_options = {from: 'contactstorpe@gmail.com'}
 #   config.action_mailer.delivery_method = :smtp
-#
- = {
+#   config.action_mailer.smtp_settings = {
 #   address:              'smtp.gmail.com',
 #   port:                 587,
 #   domain:               'gmail.com',
