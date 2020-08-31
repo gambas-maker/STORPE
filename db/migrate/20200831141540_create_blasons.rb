7cf22cfc64de7247fd2bf17800f4c6ad431df338
Clement Guibaud <clement.guibaud@kedgebs.com>
2020-08-31 16:19:57 +0200

    ajout de colonnes dans la table blason

--- db/migrate/20200831141540_create_blasons.rb
+++ db/migrate/20200831141540_create_blasons.rb
@@ -1,6 +1,8 @@
 class CreateBlasons < ActiveRecord::Migration[6.0]
   def change
     create_table :blasons do |t|
+      add_column :name, :string
+      add_column :url, :string
 
       t.timestamps
     end
