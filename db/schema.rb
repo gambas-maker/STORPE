61bddeccca0b87d29e779c5bf81f8bac6b80962e
Clement Guibaud <clement.guibaud@kedgebs.com>
2020-09-04 13:45:35 +0200

    changement dans le site map

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_01_160821) do
+ActiveRecord::Schema.define(version: 2020_09_04_085917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,6 +100,11 @@ ActiveRecord::Schema.define(version: 2020_09_01_160821) do
     t.string "event_stamp"
     t.integer "result_home"
     t.integer "result_away"
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "player_seasons", force: :cascade do |t|
