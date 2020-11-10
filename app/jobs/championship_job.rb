8e6e71083ee8a27f739c88da754ffb30eb0ca323
Clement Guibaud <clement.guibaud@kedgebs.com>
2020-11-10 09:53:44 +0100

    changement dans l'inscription, attente de test

--- app/jobs/championship_job.rb
+++ app/jobs/championship_job.rb
@@ -5,28 +5,30 @@ class ChampionshipJob < ApplicationJob
     @user = User.find(user_id)
     @playerseasons = PlayerSeason.where(championship_id: Championship.where(name: "Amateur"))
     @championships = Championship.where(name: "Amateur")
-    count = []
+    hash = {}
+    array = []
     if PlayerSeason.exists?(user_id: @user.id)
     else
       @championships.each do |championship|
-        count << championship.player_seasons.count
+        player = []
+        championship.player_seasons.each do |playerseason|
+          if playerseason.forecasts.where(season_id: Season.last.id).exists?
+            player << playerseason
+            if player.count < 20
+              puts player
+              hash[championship.id] = player.count
+              puts hash
+              array << championship
+              puts array
+            end
+          end
+        end
       end
-      array = count.reject { |number| number >= 30 }
       if array.empty?
         Championship.create(season_id: Season.last.id, name: "Amateur")
         PlayerSeason.create!(user_id: @user.id, season_id: Season.last.id, championship_id: Championship.last.id, number_of_points: 0)
       else
-        var = count.min
-        object = []
-        @championships.each do |championship|
-          if championship.player_seasons.count == var
-            object << championship
-            puts object
-          else
-          end
-        end
-        object.select { |un| un[0] }
-        PlayerSeason.create!(user_id: @user.id, season_id: Season.last.id, championship_id: object[0].id, number_of_points: 0)
+        PlayerSeason.create!(user_id: @user.id, season_id: Season.last.id, championship_id: hash.max_by{ |k,v| v }.first, number_of_points: 0)
       end
     end
   end
