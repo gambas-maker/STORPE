acff8fa2feeec534832d7af50084bbd6e3d937b3
Clement Guibaud <clement.guibaud@kedgebs.com>
2020-12-17 09:01:57 +0100

    ajout du facebook connect

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
   has_many :player_seasons
   has_many :forecasts, through: :player_seasons
   validates :email, 'valid_email_2/email': { mx: true }
@@ -11,6 +11,22 @@ class User < ApplicationRecord
   validates_uniqueness_of :pseudo
 
   after_create :mailer
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name   # assuming the user model has a name
+    end
+  end
 
   private
 
