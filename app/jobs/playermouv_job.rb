6b2f8d825787907f04117fcd2d8a2a262d67a526
Clement Guibaud <clement.guibaud@kedgebs.com>
2021-01-06 12:07:39 +0100

    ajout de la partie qui permet de connaitre le nombre de joueurs dans les divisions

--- app/jobs/playermouv_job.rb
+++ app/jobs/playermouv_job.rb
@@ -1,7 +1,28 @@
 class PlayermouvJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform
+    egal = {}
+    moins = {}
+    plus = {}
+    @amateurs = Championship.where(name: "Amateur")
+    @amateurs.each do |amateur|
+      champ = []
+      amateur.player_seasons.each do |player|
+        if player.forecasts.last.id == Season.last.id
+          champ << player
+        end
+      end
+      if champ.count < 20
+        moins[amateur] = champ.count
+      elsif champ.count > 20
+        plus[amateur] = champ.count
+      else
+        egal[amateur] = champ.count
+      end
+    end
+    puts egal
+    puts moins
+    puts plus
   end
 end
