12d1d08ece1cdba133daf150e7e10b9bf75572be
Clement Guibaud <clement.guibaud@kedgebs.com>
2021-01-07 10:24:15 +0100

    ajout pour savoir pk bug

--- app/jobs/playermouv_job.rb
+++ app/jobs/playermouv_job.rb
@@ -28,23 +28,22 @@ class PlayermouvJob < ApplicationJob
     plus.each do |champ, number|
       array = []
       x = number - 20
-      puts x
       champ.player_seasons.each do |player|
         if player.forecasts.exists? && player.forecasts.where(confirmed: true).last.season_id == Season.last.id
           array << player
         end
-        puts array
       end
       moins.each do |key, value|
+        puts array.count
         if value < 21 && array.count > 0
-          puts array.count
           x.times do
             array.first.update(championship_id: key.id)
             array.delete_at(0)
-            puts array
+            puts array.count
             moins[key] = value + 1
             puts moins
           end
+        else array.count == 0
         end
       end
     end
