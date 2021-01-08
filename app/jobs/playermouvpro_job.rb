181a07c44b64cd38adb6e2dc199319697f3caa1f
Clement Guibaud <clement.guibaud@kedgebs.com>
2021-01-08 15:18:20 +0100

    ajout des jobs pour toutes les ligues

--- app/jobs/playermouvpro_job.rb
+++ app/jobs/playermouvpro_job.rb
@@ -2,9 +2,9 @@ class PlayermouvproJob < ApplicationJob
   queue_as :default
 
   def perform
-    egal = {}
-    moins = {}
-    plus = {}
+    egalp = {}
+    moinsp = {}
+    plusp = {}
     @pros = Championship.where(name: "Pro")
     @pros.each do |pro|
       champ = []
@@ -14,41 +14,41 @@ class PlayermouvproJob < ApplicationJob
         end
       end
       if champ.count < 20
-        moins[pro] = champ.count
+        moinsp[pro] = champ.count
       elsif champ.count > 20
-        plus[pro] = champ.count
+        plusp[pro] = champ.count
       else
-        egal[pro] = champ.count
+        egalp[pro] = champ.count
       end
     end
-    plus.each do |champ, number|
+    plusp.each do |champ, number|
       array = []
       champ.player_seasons.each do |player|
         if player.forecasts.exists? && player.forecasts.where(confirmed: true).last.season_id == Season.last.id
           array << player
         end
       end
-      moins.each do |key, value|
+      moinsp.each do |key, value|
         while array.count > 20 && value < 20
           array.first.update(championship_id: key.id)
           array.delete_at(0)
           value += 1
-          moins[key] += 1
+          moinsp[key] += 1
         end
       end
     end
-    moins.each do |key, value|
+    moinsp.each do |key, value|
       if value == 20
-        egal[key] = value
-        moins.delete(key)
-      elsif value == moins.max_by { |k,v| v }.second
+        egalp[key] = value
+        moinsp.delete(key)
+      elsif value == moinsp.max_by { |k,v| v }.second
       else
         key.player_seasons.each do |player|
           if player.forecasts.exists? && player.forecasts.where(confirmed: true).last.season_id == Season.last.id && value < 20
-            puts moins.max_by { |k,v| v }.first.id
-            player.update(championship_id: moins.max_by { |k,v| v }.first.id)
+            puts moinsp.max_by { |k,v| v }.first.id
+            player.update(championship_id: moinsp.max_by { |k,v| v }.first.id)
             value += 1
-            moins[key] += 1
+            moinsp[key] += 1
           end
         end
       end
--- app/jobs/playermouvsemipro_job.rb
+++ app/jobs/playermouvsemipro_job.rb
@@ -2,9 +2,9 @@ class PlayermouvsemiproJob < ApplicationJob
   queue_as :default
 
   def perform
-    egal = {}
-    moins = {}
-    plus = {}
+    egals = {}
+    moinss = {}
+    pluss = {}
     @semis = Championship.where(name: "Semi-pro")
     @semis.each do |semi|
       champ = []
@@ -14,41 +14,41 @@ class PlayermouvsemiproJob < ApplicationJob
         end
       end
       if champ.count < 20
-        moins[semi] = champ.count
+        moinss[semi] = champ.count
       elsif champ.count > 20
-        plus[semi] = champ.count
+        pluss[semi] = champ.count
       else
-        egal[semi] = champ.count
+        egals[semi] = champ.count
       end
     end
-    plus.each do |champ, number|
+    pluss.each do |champ, number|
       array = []
       champ.player_seasons.each do |player|
         if player.forecasts.exists? && player.forecasts.where(confirmed: true).last.season_id == Season.last.id
           array << player
         end
       end
-      moins.each do |key, value|
+      moinss.each do |key, value|
         while array.count > 20 && value < 20
           array.first.update(championship_id: key.id)
           array.delete_at(0)
           value += 1
-          moins[key] += 1
+          moinss[key] += 1
         end
       end
     end
-    moins.each do |key, value|
+    moinss.each do |key, value|
       if value == 20
-        egal[key] = value
-        moins.delete(key)
-      elsif value == moins.max_by { |k,v| v }.second
+        egals[key] = value
+        moinss.delete(key)
+      elsif value == moinss.max_by { |k,v| v }.second
       else
         key.player_seasons.each do |player|
           if player.forecasts.exists? && player.forecasts.where(confirmed: true).last.season_id == Season.last.id && value < 20
-            puts moins.max_by { |k,v| v }.first.id
-            player.update(championship_id: moins.max_by { |k,v| v }.first.id)
+            puts moinss.max_by { |k,v| v }.first.id
+            player.update(championship_id: moinss.max_by { |k,v| v }.first.id)
             value += 1
-            moins[key] += 1
+            moinss[key] += 1
           end
         end
       end
