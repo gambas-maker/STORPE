bcab540e60435a8862b1b30650bfd0dd43cfe620
Clement Guibaud <clement.guibaud@kedgebs.com>
2021-01-12 19:17:31 +0100

    nouveau test pour changer type de column pour les nouvelles cotes

--- db/migrate/20210112180314_change_column_matches.rb
+++ db/migrate/20210112180314_change_column_matches.rb
@@ -1,13 +1,13 @@
 class ChangeColumnMatches < ActiveRecord::Migration[6.0]
   def change
-    change_column :matches, :buteur_1, :float, "USING buteur_1::double precision"
-    change_column :matches, :buteur_2, :float, "USING buteur_2::double precision"
-    change_column :matches, :buteur_3, :float, "USING buteur_3::double precision"
-    change_column :matches, :buteur_4, :float, "USING buteur_4::double precision"
-    change_column :matches, :over_05, :float, "USING over_05::double precision"
-    change_column :matches, :over_15, :float, "USING over_15::double precision"
-    change_column :matches, :under_05, :float, "USING under_05::double precision"
-    change_column :matches, :under_15, :float, "USING under_15::double precision"
-    change_column :matches, :goal_two_teams, :float, "USING goal_two_teams::double precision"
+    change_column :matches, :buteur_1, :float, "float USING CAST(buteur_1 AS float)"
+    change_column :matches, :buteur_2, :float, "float USING CAST(buteur_2 AS float)"
+    change_column :matches, :buteur_3, :float, "float USING CAST(buteur_3 AS float)"
+    change_column :matches, :buteur_4, :float, "float USING CAST(buteur_4 AS float)"
+    change_column :matches, :over_05, :float, "float USING CAST(over_05 AS float)"
+    change_column :matches, :over_15, :float, "float USING CAST(over_15 AS float)"
+    change_column :matches, :under_05, :float, "float USING CAST(under_05 AS float)"
+    change_column :matches, :under_15, :float, "float USING CAST(under_15 AS float)"
+    change_column :matches, :goal_two_teams, :float, "float USING CAST(goal_two_teams AS float)"
   end
 end
