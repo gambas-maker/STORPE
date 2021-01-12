a7341ec360f9c8e15c6814aaa5b9ad9d527518ac
Clement Guibaud <clement.guibaud@kedgebs.com>
2021-01-12 19:32:46 +0100

    typo lors de la migration

--- db/migrate/20210112183002_add_column_to_matches.rb
+++ db/migrate/20210112183002_add_column_to_matches.rb
@@ -1,6 +1,6 @@
 class AddColumnToMatches < ActiveRecord::Migration[6.0]
   def change
-    add_column :matches, :goals_two_teams_no, :float
-    rename_column :matches, :goals_two_teams, :goals_two_teams_yes
+    add_column :matches, :goal_two_teams_no, :float
+    rename_column :matches, :goal_two_teams, :goals_two_teams_yes
   end
 end
