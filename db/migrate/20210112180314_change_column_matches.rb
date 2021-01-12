f8a4687677fb4b08a1ab227036eecadbd22c4a6c
Clement Guibaud <clement.guibaud@kedgebs.com>
2021-01-12 19:18:26 +0100

    ajout

--- db/migrate/20210112180314_change_column_matches.rb
+++ db/migrate/20210112180314_change_column_matches.rb
@@ -1,13 +1,13 @@
 class ChangeColumnMatches < ActiveRecord::Migration[6.0]
   def change
-    change_column :matches, :buteur_1, :float, "float USING CAST(buteur_1 AS float)"
-    change_column :matches, :buteur_2, :float, "float USING CAST(buteur_2 AS float)"
-    change_column :matches, :buteur_3, :float, "float USING CAST(buteur_3 AS float)"
-    change_column :matches, :buteur_4, :float, "float USING CAST(buteur_4 AS float)"
-    change_column :matches, :over_05, :float, "float USING CAST(over_05 AS float)"
-    change_column :matches, :over_15, :float, "float USING CAST(over_15 AS float)"
-    change_column :matches, :under_05, :float, "float USING CAST(under_05 AS float)"
-    change_column :matches, :under_15, :float, "float USING CAST(under_15 AS float)"
-    change_column :matches, :goal_two_teams, :float, "float USING CAST(goal_two_teams AS float)"
+    change_column :matches, :buteur_1, :float
+    change_column :matches, :buteur_2, :float
+    change_column :matches, :buteur_3, :float
+    change_column :matches, :buteur_4, :float
+    change_column :matches, :over_05, :float
+    change_column :matches, :over_15, :float
+    change_column :matches, :under_05, :float
+    change_column :matches, :under_15, :float
+    change_column :matches, :goal_two_teams, :float
   end
 end
