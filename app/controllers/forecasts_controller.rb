d8eca536f6d0668f0ce1cd66d377a0bda0ae10f9
Clement Guibaud <clement.guibaud@kedgebs.com>
2021-01-14 18:32:51 +0100

    changement forecast

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -51,6 +51,10 @@ class ForecastsController < ApplicationController
         elsif @forecast.present?
           if @forecast.outcome == "1" || "NULL" || "2"
             @forecast.update(outcome: outcome)
+          elsif forecast.outcome == "7" || "8"
+            @forecast.update(outcome: outcome)
+          elsif forecast.outcome == "9" || "10"
+            @forecast.update(outcome: outcome)
           else
             @forecast = Forecast.create(match_id: id, player_season_id: player[0], points_lose: 0, points_win: 0, season_id: Season.last.id, confirmed: false, outcome: outcome)
           end
