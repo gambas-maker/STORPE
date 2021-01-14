62e4668267bd14ba810149cd2a4de9f412c8a6f3
Clement Guibaud <clement.guibaud@kedgebs.com>
2021-01-14 15:20:52 +0100

    changement de la calcul des résultats

--- app/jobs/calculatepoints_job.rb
+++ app/jobs/calculatepoints_job.rb
@@ -24,6 +24,22 @@ class CalculatepointsJob < ApplicationJob
             forecast.update(points_lose: match.negative_points_home)
           elsif match.result[0].to_i < match.result[2].to_i && forecast.outcome == "1"
             forecast.update(points_lose: match.negative_points_home)
+          elsif match.result[0].to_i > 0 && match.result[2].to_i > 0 && forecast.outcome == "7"
+            forecast.update(points_win: match.goal_two_teams_yes)
+          elsif match.result[0].to_i < 1 || match.result[2].to_i < 1 && forecast.outcome == "7"
+            forecast.update(points_lose: match.negative_goal_two_teams_yes)
+          elsif match.result[0].to_i > 0 && match.result[2].to_i > 0 && forecast.outcome == "8"
+            forecast.update(points_lose: match.negative_goal_two_teams_no)
+          elsif match.result[0].to_i < 0 || match.result[2].to_i < 0 && forecast.outcome == "8"
+            forecast.update(points_win: match.goal_two_teams_no)
+          elsif match.result[0].to_i + match.result[2].to_i > 2 && forecast.outcome == "9"
+            forecast.update(points_lose: match.negative_under25)
+          elsif match.result[0].to_i + match.result[2].to_i < 3 && forecast.outcome == "9"
+            forecast.update(points_win: match.under25)
+          elsif match.result[0].to_i + match.result[2].to_i > 2 && forecast.outcome == "10"
+            forecast.update(points_win: match.over_25)
+          elsif match.result[0].to_i + match.result[2].to_i < 3 && forecast.outcome == "10"
+            forecast.update(points_lose: match.negative_over25)
           end
         end
       end
