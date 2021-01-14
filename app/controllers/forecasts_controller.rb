fe4912056bcdce29394ebe9a826498c1782a99b6
Clement Guibaud <clement.guibaud@kedgebs.com>
2021-01-14 18:50:51 +0100

    changement

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -48,16 +48,14 @@ class ForecastsController < ApplicationController
           @forecast.season_id = Season.last.id
           @forecast.confirmed = false
           @forecast.save!
-        elsif @forecast.present?
-          if @forecast.outcome == "1" || "NULL" || "2"
-            @forecast.update(outcome: outcome)
-          elsif forecast.outcome == "7" || "8"
-            @forecast.update(outcome: outcome)
-          elsif forecast.outcome == "9" || "10"
-            @forecast.update(outcome: outcome)
-          else
-            @forecast = Forecast.create(match_id: id, player_season_id: player[0], points_lose: 0, points_win: 0, season_id: Season.last.id, confirmed: false, outcome: outcome)
-          end
+        elsif @forecast.present? && @forecast.outcome == "1" || "NULL" || "2"
+          @forecast.update(outcome: outcome)
+        elsif @forecast.present? && forecast.outcome == "7" || "8"
+          @forecast.update(outcome: outcome)
+        elsif @forecast.present? && forecast.outcome == "9" || "10"
+          @forecast.update(outcome: outcome)
+        # elsif
+        #   @forecast = Forecast.create(match_id: id, player_season_id: player[0], points_lose: 0, points_win: 0, season_id: Season.last.id, confirmed: false, outcome: outcome)
         else
           @forecast.outcome = outcome
           @forecast.save
