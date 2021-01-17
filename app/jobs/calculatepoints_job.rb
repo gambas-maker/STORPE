7eacf7fc987f62ee6f9cfd638e39715a3c024c75
Clement Guibaud <clement.guibaud@kedgebs.com>
2021-01-17 09:36:53 +0100

    test

--- app/jobs/calculatepoints_job.rb
+++ app/jobs/calculatepoints_job.rb
@@ -24,13 +24,13 @@ class CalculatepointsJob < ApplicationJob
             forecast.update(points_lose: match.negative_points_home)
           elsif match.result[0].to_i < match.result[2].to_i && forecast.outcome == "1"
             forecast.update(points_lose: match.negative_points_home)
-          elsif match.result[0].to_i > 0 && match.result[2].to_i > 0 && forecast.outcome == "7"
+          elsif forecast.outcome == "7" && match.result[0].to_i > 0 && match.result[2].to_i > 0
             forecast.update(points_win: match.goal_two_teams_yes)
-          elsif match.result[0].to_i < 1 || match.result[2].to_i < 1 && forecast.outcome == "7"
+          elsif forecast.outcome == "7" && match.result[0].to_i < 1 || match.result[2].to_i < 1
             forecast.update(points_lose: match.negative_goal_two_teams_yes)
-          elsif match.result[0].to_i > 0 && match.result[2].to_i > 0 && forecast.outcome == "8"
+          elsif forecast.outcome == "8" && match.result[0].to_i > 0 && match.result[2].to_i > 0
             forecast.update(points_lose: match.negative_goal_two_teams_no)
-          elsif match.result[0].to_i == 0 || match.result[2].to_i == 0 && forecast.outcome == "8"
+          elsif forecast.outcome == "8" && match.result[0].to_i == 0 || match.result[2].to_i == 0
             forecast.update(points_win: match.goal_two_teams_no)
           elsif match.result[0].to_i + match.result[2].to_i > 2 && forecast.outcome == "9"
             forecast.update(points_lose: match.negative_under25)
