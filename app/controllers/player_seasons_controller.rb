b18495a19dded06d21bac572fa63b17a9fb57ef0
Clement Guibaud <clement.guibaud@kedgebs.com>
2021-01-20 18:15:22 +0100

    ajout front

--- app/controllers/player_seasons_controller.rb
+++ app/controllers/player_seasons_controller.rb
@@ -2,6 +2,7 @@ class PlayerSeasonsController < ApplicationController
   def show
     @forecasts = Forecast.where(confirmed: true)
     @championship = Championship.where(id: current_user.player_seasons[0].championship.id)
+    @seasons = Season.all
   end
 
   def edit
