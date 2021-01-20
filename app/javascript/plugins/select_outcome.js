97564c362ee7a3ca5d1862d8ced6fcf01845f26c
Clement Guibaud <clement.guibaud@kedgebs.com>
2021-01-20 10:55:20 +0100

    test8

--- app/javascript/plugins/select_outcome.js
+++ app/javascript/plugins/select_outcome.js
@@ -114,7 +114,7 @@ const calculate = () => {
     const parent = tgt.closest(".displaysquares");
     var x = document.querySelectorAll(".square:checked").length; console.log(x);
     document.querySelector(".plus").innerHTML = z;
-  } else if {
+  } else if (tgt.classList.contains("square_b2e")) {
     const parent = tgt.closest(".b2e");
     var y = document.querySelectorAll(".square_b2e:checked").length; console.log(y);
     document.querySelector(".plus").innerHTML = z;
