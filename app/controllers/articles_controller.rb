da08772f9338519c83b6dffac29bb94827921110
Clement Guibaud <clement.guibaud@kedgebs.com>
2021-01-26 10:32:12 +0100

    ajout migration avec calomun pour image dans article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,11 @@
 class ArticlesController < ApplicationController
-
+  skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -23,11 +23,9 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was updated"
       redirect_to article_path(@article)
@@ -37,9 +35,19 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article.destroy
+    flash[:notice] = "Article was deleted"
+    redirect_to articles_path
+  end
+
   private
 
   def article_params
     params.require(:article).permit(:title, :description, :text)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
---
+++ db/migrate/20210126092941_add_column_image.rb
@@ -0,0 +1,5 @@
+class AddColumnImage < ActiveRecord::Migration[6.0]
+  def change
+    add_column :articles, :url, :string
+  end
+end
