8dd274c406f6d05407041ceb117c3feee55dc83c
Clement Guibaud <clement.guibaud@kedgebs.com>
2021-01-26 09:35:01 +0100

    ajout

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
 
+  def index
+  @articles = Article.all
+  end
+
   def show
     @article = Article.find(params[:id])
   end
@@ -19,6 +23,18 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated"
+      redirect_to article_path(@article)
+    else
+      flash[:notice] = "Article was not updated"
+      render :edit
+    end
   end
 
   private
