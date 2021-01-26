d06dabe95ea51022ed858ca43722e5ff410db01b
Clement Guibaud <clement.guibaud@kedgebs.com>
2021-01-26 17:04:21 +0100

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_094450) do
+ActiveRecord::Schema.define(version: 2021_01_26_155451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,17 @@ ActiveRecord::Schema.define(version: 2021_01_26_094450) do
     t.date "start_date"
     t.date "end_date"
     t.index ["season_id"], name: "index_championships_on_season_id"
+  end
+
+  create_table "ckeditor_assets", force: :cascade do |t|
+    t.string "data_file_name", null: false
+    t.string "data_content_type"
+    t.integer "data_file_size"
+    t.string "data_fingerprint"
+    t.string "type", limit: 30
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["type"], name: "index_ckeditor_assets_on_type"
   end
 
   create_table "forecasts", force: :cascade do |t|
