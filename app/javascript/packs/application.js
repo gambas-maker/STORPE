7c44f6713fa7685d50f84679129a5deef8db9075
Clement Guibaud <clement.guibaud@kedgebs.com>
2021-01-27 08:28:55 +0100

    ajout test de action text

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,6 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-//= require tinymce
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
@@ -38,3 +37,6 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
 });
+
+require("trix")
+require("@rails/actiontext")
