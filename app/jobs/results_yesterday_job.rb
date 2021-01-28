3e074236451eb14a0286b129d438efbb1e67796c
Clement Guibaud <clement.guibaud@kedgebs.com>
2021-01-28 08:46:40 +0100

    ajout d'un job pour calculer les points des matchs d'hier

--- app/jobs/results_yesterday_job.rb
+++ app/jobs/results_yesterday_job.rb
@@ -1,7 +1,13 @@
 class ResultsYesterdayJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform
+    puts "Let's get the football results"
+    @matches = Match.where(event_stamp: Date.yesterday.to_s, sport: "football")
+    @matches.each do |game|
+      SportOdd.get_results_for_match(game)
+    end
+    CalculatepointsJob.perform_now
+    RecalcpointsJob.perform_now
   end
 end
