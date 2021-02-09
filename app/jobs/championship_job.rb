3a988c73ff458caac9458b3b353db4ec41bc1683
Clement Guibaud <clement.guibaud@kedgebs.com>
2021-02-09 10:57:43 +0100

    modification dans championship_job, création d'un compte et assignation du championnat amateur de référence'

--- app/jobs/championship_job.rb
+++ app/jobs/championship_job.rb
@@ -3,33 +3,9 @@ class ChampionshipJob < ApplicationJob
 
   def perform(user_id)
     @user = User.find(user_id)
-    @playerseasons = PlayerSeason.where(championship_id: Championship.where(name: "Amateur"))
-    @championships = Championship.where(name: "Amateur")
-    hash = {}
-    array = []
     if PlayerSeason.exists?(user_id: @user.id)
     else
-      @championships.each do |championship|
-        player = []
-        championship.player_seasons.each do |playerseason|
-          if playerseason.forecasts.where(season_id: Season.last.id).exists?
-            player << playerseason
-          end
-        end
-        if player.count < 20
-          puts player
-          hash[championship.id] = player.count
-          puts hash
-          array << championship
-          puts array
-        end
-      end
-      if array.empty?
-        Championship.create(season_id: Season.last.id, name: "Amateur")
-        PlayerSeason.create!(user_id: @user.id, season_id: Season.last.id, championship_id: Championship.last.id, number_of_points: 0)
-      else
-        PlayerSeason.create!(user_id: @user.id, season_id: Season.last.id, championship_id: hash.max_by{ |k,v| v }.first, number_of_points: 0)
-      end
+      PlayerSeason.create!(user_id: @user.id, season_id: Season.last.id, championship_id: 11, number_of_points: 0)
+    end
   end
-end
 end
