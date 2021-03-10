ae534730ec927a7ecdb095a098298547081b4058
Clement Guibaud <clement.guibaud@kedgebs.com>
2021-03-10 12:35:55 +0100

    ajout job pour replacement joueuers

---
+++ app/jobs/playermouvchampion_job.rb
@@ -0,0 +1,41 @@
+class PlayermouvchampionJob < ApplicationJob
+  queue_as :default
+
+  def perform
+    egalp = {}
+    moinsp = {}
+    plusp = {}
+    @champions = Championship.where(name: "Champion")
+    @champions.each do |champion|
+      champ = []
+      pro.player_seasons.each do |player|
+        if player.season_id == Season.last.id
+          champ << player
+        end
+      end
+      if champ.count < 20
+        moinsp[pro] = champ.count
+      elsif champ.count > 20
+        plusp[pro] = champ.count
+      else
+        egalp[pro] = champ.count
+      end
+    end
+    plusp.each do |champ, number|
+      array = []
+      champ.player_seasons.each do |player|
+        if player.season_id == Season.last.id
+          array << player
+        end
+      end
+      moinsp.each do |key, value|
+        while array.count > 20 && value < 20
+          array.first.update(championship_id: key.id)
+          array.delete_at(0)
+          value += 1
+          moinsp[key] += 1
+        end
+      end
+    end
+  end
+end
