4241879e213290ee4d746c758a541e4e2edb64f1
Clement Guibaud <clement.guibaud@kedgebs.com>
2021-04-05 17:51:59 +0200

    migration pour les helpers et la table nouvellement créée

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_05_154851) do
+ActiveRecord::Schema.define(version: 2021_04_05_155060) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,6 +105,17 @@ ActiveRecord::Schema.define(version: 2021_04_05_154851) do
     t.index ["match_id"], name: "index_forecasts_on_match_id"
     t.index ["player_season_id"], name: "index_forecasts_on_player_season_id"
     t.index ["season_id"], name: "index_forecasts_on_season_id"
+  end
+
+  create_table "friendships", id: :serial, force: :cascade do |t|
+    t.string "friendable_type"
+    t.integer "friendable_id"
+    t.integer "friend_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer "blocker_id"
+    t.integer "status"
+    t.index ["friendable_id", "friend_id"], name: "index_friendships_on_friendable_id_and_friend_id", unique: true
   end
 
   create_table "matches", force: :cascade do |t|
