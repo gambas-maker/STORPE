db5271708cd5e20ad6d94995d8bca60c3442a2af
Clement Guibaud <clement.guibaud@kedgebs.com>
2021-04-05 17:46:05 +0200

    test

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,11 @@ end
   get 'store_outcome_striker1', to: 'forecasts#store_outcome_striker1', defaults: {format: :json}
   get 'confirm_pending', to: 'forecasts#confirm_pending', defaults: {format: :json}
   get '/sitemap.xml.gz', to: redirect("https://s3-eu-west-3.amazonaws.com/storpesitemap/sitemap.xml.gz")
+  post "/friends/add" => "friends/add"
+  post "/friends/reject" => "friends/reject"
+  post "/friends/remove" => "friends/remove"
+  get "/friends/search" => "friends/search"
+  post "/friends/search" => "friends/search"
 
   resources :pages, only: [:show]
   resources :articles
@@ -31,5 +36,6 @@ end
   resources :forecasts, only: [:destroy]
   resources :seasons, only: [:show]
   resources :player_seasons
+  resources :friends, only: [:index, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
