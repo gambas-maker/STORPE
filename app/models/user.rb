df987dfd13df74dc5d4a4c98c5a58f136697a9da
Clement Guibaud <clement.guibaud@kedgebs.com>
2021-04-05 17:59:33 +0200

    front

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,35 @@ class User < ApplicationRecord
   validates :pseudo, presence: true, length: {maximum: 12}
   after_commit :async_update
   validates_uniqueness_of :pseudo
+  has_friendship
 
   after_create :mailer
+
+  def friends?
+        self.friends
+    end
+
+    def friend_requests?
+        self.requested_friends.any?
+    end
+
+    def requested_friends?
+        self.pending_friends.any?
+    end
+
+    def invite_friend(user)
+        self.friend_request(user)
+    end
+
+    def not_friends
+        potential = []
+        User.all.each do |user|
+            if(self.friends_with?(user) != true && self != user && self.friends.include?(user) != true && self.pending_friends.include?(user) != true && self.requested_friends.include?(user) != true)
+                potential << user
+            end
+        end
+        potential
+    end
 
   # def self.new_with_session(params, session)
   #   super.tap do |user|
