ee540f7766a7ade4868c84ad7ccd9b16acd269ff
Clement Guibaud <clement.guibaud@kedgebs.com>
2021-04-07 14:50:43 +0200

    test

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -4,4 +4,9 @@ class SeasonsController < ApplicationController
     @championship = Championship.where(id: current_user.player_seasons[0].championship.id)
     @playerseasons = PlayerSeason.where(championship_id: current_user.player_seasons[0].championship.id, season_id: Season.last.id)
   end
+
+  def edit
+    @playerseason = PlayerSeason.where(user_id: current_user.id)
+    @forecasts = Forecast.where(season_id: Season.last.id, player_season_id: current_user.player_seasons[0].id)
+  end
 end
