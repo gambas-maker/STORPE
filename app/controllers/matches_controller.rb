0df8249f8b713781e0b5600c50ce6a6b44f3037f
Clement Guibaud <clement.guibaud@kedgebs.com>
2021-04-08 16:04:45 +0200

    test

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -5,12 +5,6 @@ class MatchesController < ApplicationController
     league_filters
   end
 
-  def edit
-    @playerseason = PlayerSeason.where(user_id: current_user.id)
-    # @forecasts = Forecast.where(season_id: Season.last.id, player_season_id: current_user.player_seasons[0].id)
-    @matches = Match.select { |v| v.event_stamp == Date.today.to_s || v.event_stamp == Date.tomorrow.to_s || v.event_stamp == (Date.tomorrow + 1).to_s }
-  end
-
   def league_filters
     if params[:query].present?
       @matches = Match.where(league: params[:query])
--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -4,10 +4,4 @@ class SeasonsController < ApplicationController
     @championship = Championship.where(id: current_user.player_seasons[0].championship.id)
     @playerseasons = PlayerSeason.where(championship_id: current_user.player_seasons[0].championship.id, season_id: Season.last.id)
   end
-
-  def edit
-    @playerseason = PlayerSeason.where(user_id: current_user.id)
-    # @forecasts = Forecast.where(season_id: Season.last.id, player_season_id: current_user.player_seasons[0].id)
-    @matches = Match.select { |v| v.event_stamp == Date.today.to_s || v.event_stamp == Date.tomorrow.to_s || v.event_stamp == (Date.tomorrow + 1).to_s }
-  end
 end
