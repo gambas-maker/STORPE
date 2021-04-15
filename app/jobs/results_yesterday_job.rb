139af48d52a1c881dbf689824961990d8e3dd72a
Clement Guibaud <clement.guibaud@kedgebs.com>
2021-04-15 09:46:13 +0200

    retour normal

--- app/jobs/results_yesterday_job.rb
+++ app/jobs/results_yesterday_job.rb
@@ -3,7 +3,7 @@ class ResultsYesterdayJob < ApplicationJob
 
   def perform
     puts "Let's get the football results"
-    @matches = Match.where(event_stamp: (Date.yesterday - 1).to_s, sport: "football")
+    @matches = Match.where(event_stamp: Date.yesterday.to_s, sport: "football")
     @matches.each do |game|
       SportOdd.get_results_for_match(game)
     end
