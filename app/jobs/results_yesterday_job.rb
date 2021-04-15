7eb5edb3f69511a8ba1b1ca39e3f7c12be7b57b1
Clement Guibaud <clement.guibaud@kedgebs.com>
2021-04-15 09:31:49 +0200

    oubli fecth match mbappe mardi

--- app/jobs/results_yesterday_job.rb
+++ app/jobs/results_yesterday_job.rb
@@ -3,7 +3,7 @@ class ResultsYesterdayJob < ApplicationJob
 
   def perform
     puts "Let's get the football results"
-    @matches = Match.where(event_stamp: Date.yesterday.to_s, sport: "football")
+    @matches = Match.where(event_stamp: Date.yesterday.to_s - 1, sport: "football")
     @matches.each do |game|
       SportOdd.get_results_for_match(game)
     end
