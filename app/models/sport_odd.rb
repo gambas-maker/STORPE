f2f5444a33cd1308ee85acccd560400a5f2b75ef
Clement Guibaud <clement.guibaud@kedgebs.com>
2021-04-19 11:30:27 +0200

    ajout des matchs de FA cup et Coupe de france

--- app/models/sport_odd.rb
+++ app/models/sport_odd.rb
@@ -5,8 +5,8 @@ require 'json'
 
 class SportOdd
   BASE_URL = "https://v2.api-football.com/"
-  LEAGUE_IDS = [2664, 2790, 2833, 2857, 2755, 2771, 2777, 1422, 925, 1073, 1252, 3079, 3026]
-  # 2664 = France, 2790 = Angleterre, 2833 = Espagne, 2857 = Italie, 2755 = Allemagne, 2771 = Champions League, 2777 = Europa League, 1422 = Nations League, Bottola Pro = 925, CAN = 1073, World Cup South-Am-Qualif = 1252, World Cup - Qualification Europe = 3079, Women LDC = 3026
+  LEAGUE_IDS = [2664, 2790, 2833, 2857, 2755, 2771, 2777, 1422, 925, 1073, 1252, 3079, 3026, 3081, 2791]
+  # 2664 = France, 2790 = Angleterre, 2833 = Espagne, 2857 = Italie, 2755 = Allemagne, 2771 = Champions League, 2777 = Europa League, 1422 = Nations League, Bottola Pro = 925, CAN = 1073, World Cup South-Am-Qualif = 1252, World Cup - Qualification Europe = 3079, Women LDC = 3026, Coupe de France = 3081, FA Cup = 2791
 
   def self.matches_for_four_days
     LEAGUE_IDS.each do |league_id|
