5c60b98dae9ebbdb802ef46ae4028dda1eb00f64
Clement Guibaud <clement.guibaud@kedgebs.com>
2021-04-19 12:22:30 +0200

    test

--- app/jobs/resultsbaskettoday_job.rb
+++ app/jobs/resultsbaskettoday_job.rb
@@ -3,7 +3,7 @@ class ResultsbaskettodayJob < ApplicationJob
 
   def perform
     puts "Let's get the basketball results"
-    @matches = Match.where(event_stamp: Date.today.to_s, sport: "basketball")
+    @matches = Match.where(event_stamp: "2021-04-14", sport: "basketball")
     @matches.each do |game|
       BasketballTomorrow.get_results_for_match(game)
     end
