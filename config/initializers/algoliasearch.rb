58c6f6a9f1ab6edefcb0db5e95e3b2646ed7d3b8
Clement Guibaud <clement.guibaud@kedgebs.com>
2021-04-23 18:27:06 +0200

    changement des vars

--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -1,1 +1,1 @@
-AlgoliaSearch.configuration = { application_id: ENV['APPLICATION_ID'], api_key: ENV['API_KEY'] }
+AlgoliaSearch.configuration = { application_id: ENV['ALGOLIASEARCH_APPLICATION_ID'], api_key: ENV['ALGOLIASEARCH_API_KEY'] }
