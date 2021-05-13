6bc5333737c7057caa001f46076fc97683c342fb
Clement Guibaud <clement.guibaud@kedgebs.com>
2021-05-13 17:46:35 +0200

    migration ajout colonne club dans les blasons

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_13_130949) do
+ActiveRecord::Schema.define(version: 2021_05_13_154443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2021_05_13_130949) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.string "url"
+    t.integer "club"
   end
 
   create_table "championships", force: :cascade do |t|
