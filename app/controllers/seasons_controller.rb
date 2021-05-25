a1192b0b9cd270fcd50c6622a7eaa7cf59ad4816
Clement Guibaud <clement.guibaud@kedgebs.com>
2021-05-25 14:51:06 +0200

    ajout front pour augmenter la vitesse

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -3,5 +3,9 @@ class SeasonsController < ApplicationController
     @playerseason = PlayerSeason.where(user_id: current_user.id)
     @championship = Championship.where(id: current_user.player_seasons[0].championship.id)
     @playerseasons = PlayerSeason.where(championship_id: current_user.player_seasons[0].championship.id, season_id: Season.last.id)
+    if current_user.club == "LOSC"
+      @userslosc = User.where(club: "LOSC")
+    else
+    end
   end
 end
